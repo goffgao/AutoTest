3df975a29b560c003d5a2dc22fa1ba29d7dc3ae6
goff <goffgao@163.com>
2021-05-29 15:44:13 +0800

    if和else if和else用法

--- ChapterJava/src/main/java/com/ifandelse/IfTest.java
+++ ChapterJava/src/main/java/com/ifandelse/IfTest.java
@@ -2,7 +2,7 @@ package com.ifandelse;
 
 public class IfTest {
     public static void main(String[] args) {
-        System.out.println("执行语法if或者else中的内容");
+        System.out.println("执行语法if或者else中的内容==========");
         int x =1;
         if (x == 2){
             System.out.println("if语法体");
@@ -11,12 +11,27 @@ public class IfTest {
         }
 
 
-        System.out.println("算法计算");
+        System.out.println("算法计算============");
         int a =13;
         if (a % 3 ==0){//如果除以2能够余数为0,说明是偶数
             System.out.println("偶数");}
         else {
             System.out.println("奇数");
         }
+
+        System.out.println("使用if后++++++++else  if+++else if+++++++else");
+        int b =30;
+        int c;
+        if (b>=3){
+            c = b *3;
+            System.out.println("条件1=="+c);
+        }
+        else if( b<=3){
+            c = b/3;
+            System.out.println("条件2=="+c);
+
+        }
+        else {
+            System.out.println( "结束条件了"); }
     }
 }
