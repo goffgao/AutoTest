4c9a8c7dfc8b1b1b3782d5800cb647256025e60e
goff <goffgao@163.com>
2021-05-29 15:29:08 +0800

    if else用啊

---
+++ ChapterJava/src/main/java/com/ifandelse/IfTest.java
@@ -0,0 +1,22 @@
+package com.ifandelse;
+
+public class IfTest {
+    public static void main(String[] args) {
+        System.out.println("执行语法if或者else中的内容");
+        int x =1;
+        if (x == 2){
+            System.out.println("if语法体");
+        }else{
+            System.out.println("else语法体");
+        }
+
+
+        System.out.println("算法计算");
+        int a =13;
+        if (a % 3 ==0){//如果除以2能够余数为0,说明是偶数
+            System.out.println("偶数");}
+        else {
+            System.out.println("奇数");
+        }
+    }
+}
