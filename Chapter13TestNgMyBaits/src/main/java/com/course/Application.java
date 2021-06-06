43ee0212eb618b70174d08036ca924ca6bc8432e
gaofei <@123456Qw>
2021-06-06 21:39:11 +0800

    atest

--- Chapter13TestNgMyBaits/src/main/java/com/course/Application.java
+++ Chapter13TestNgMyBaits/src/main/java/com/course/Application.java
@@ -1,7 +1,6 @@
 package com.course;
 
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
--- ChapterWT/src/main/java/cn/wt/Test.java
+++ ChapterWT/src/main/java/cn/wt/Test.java
@@ -3,7 +3,7 @@ package cn.wt;
 public class Test {
     public static void main(String[] args) {
         {
-
+            System.out.println("r");
         }
     }
 }
